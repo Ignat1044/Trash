ef8a6800ddd8c40530352fe9476eef3a0fd69a48
Данило Гнатюк <danilognatuk@MacBook-Air-Danilo.local>
2023-05-25 13:48:02 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Items from './components/Items';
+import { Dialog } from '@headlessui/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      orders:[],
+      items:[
+      {
+        id:0,
+        title:'Chair Gray',
+        img:'gray_chair.jpeg',
+        desc:'pam pam pam grey chair pam pam',
+        category:'chairs',
+        price:'49.99'
+      },
+      {
+        id:2,
+        title:'Chair Gray',
+        img:'gray_chair.jpeg',
+        desc:'pam pam pam grey chair pam pam',
+        category:'chairs',
+        price:'79.99'
+      }
+
+
+      ]
+    }
+    this.addToOrder=this.addToOrder.bind(this)
+    this.delateOrder=this.delateOrder.bind(this)
+
+  }
+  render(){
+    return (
+      <div className='wrapper'>
+        <Header orders={this.state.orders} onDelate={this.delateOrder}/>
+        {/* <Dialog/> */}
+        <Items items={this.state.items} onAdd={this.addToOrder}/>
+        <Footer/>
+      </div>
+    );
+  }
+  delateOrder(id){
+    this.setState({orders: this.state.orders.filter(el=>el.id!==id )})
+  }
+  addToOrder(item){
+    let isInArray=false
+    this.state.orders.forEach(el => {
+      if(el.id===item.id){
+        isInArray=true
+      }
+    })
+    if(!isInArray){
+      this.setState({orders:[...this.state.orders, item]})
+    }
+    
+  }
 }
+
+
 
 export default App;
